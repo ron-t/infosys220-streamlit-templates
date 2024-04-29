ac6be0adacbec0a2e03404be17c9e1db8d6eb39f
ron-t <17290359+ron-t@users.noreply.github.com>
2024-04-29 16:55:32 +1200

    simplify 0_Read_public_gsheet exercise

--- pages/10_Read_public_gsheet.py
+++ pages/10_Read_public_gsheet.py
@@ -3,36 +3,20 @@ from streamlit_gsheets import GSheetsConnection
 
 st.title("Read data from a public google sheet")
 
-# We use a form to control when the page is (re)loaded and hence when the sheet is retrieved.
-with st.form("form"):
-    # Accept url as user input
-    url = st.text_input(
-        "URL of the public googlesheet. Include the #gid=... part of the URL.",
-        value="paste URL here",
-    )
+# Accept url as user input
+url = st.text_input(
+    "URL of the public googlesheet. Include the #gid=... part of the URL.",
+    value="paste URL here",
+)
 
-    # header is a pandas.read_csv parameter. We pass either None or 0. Behaviour is described here: https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.read_csv.html
-    header_row = None
-    use_first_row_as_header = st.checkbox("Use first row as header?")
-    if use_first_row_as_header:
-        header_row = 0
+conn = st.connection(
+    "gsheets",
+    type=GSheetsConnection,
+)
 
-    submitted = st.form_submit_button("Retrieve")
+df = conn.read(spreadsheet=url)  # edit this to store the result in a dataframe
 
-if submitted:
-    # Create a connection object.
-    conn = st.connection(
-        "gsheets",
-        type=GSheetsConnection,
-    )
-
-    df = conn.read(
-        spreadsheet=url,
-        header=header_row,
-    )
-
-    # Show data using st.dataframe.
-    st.dataframe(
-        df,
-        hide_index=True,
-    )
+st.dataframe(
+    df,  # edit this to show the dataframe from above
+    hide_index=True,
+)
